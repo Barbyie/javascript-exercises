6749b5d5aaeda94efee5cd4cfcb45c84cfa84810
Barbyie <thiago.barbieri2002@gmail.com>
2024-02-07 15:14:45 -0300

    Finish second test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,12 +1,10 @@
 const removeFromArray = function(array, value1, value2) {
-  let newArray = 0;
-  for (let i = 0; i <= array.length - 1; i++) {
+  for (let i =array.length - 1; i >=0; i--) {
     if (array[i] === value1 || array[i] === value2) {
       array.splice(i, 1);
     };
   };
   return array;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
