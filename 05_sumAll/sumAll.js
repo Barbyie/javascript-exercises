d8193969f3663df511b3d08ea8e9df3b4c7d1902
Barbyie <thiago.barbieri2002@gmail.com>
2024-02-07 21:15:06 -0300

    Pass first test

---
+++ 05_sumAll/sumAll.js
@@ -0,0 +1,11 @@
+const sumAll = function(arg1, arg2) {
+  let sum = 0;
+  for (let i = arg1 ; i <= arg2; i++) {
+    sum += (i);
+  };
+  return sum;
+
+};
+
+// Do not edit below this line
+module.exports = sumAll;
