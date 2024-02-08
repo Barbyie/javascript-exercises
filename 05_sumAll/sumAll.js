98cfe3ee8a7735dc78ad5b421c7f3ec607d88e65
Barbyie <thiago.barbieri2002@gmail.com>
2024-02-07 21:28:15 -0300

    Pass third test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,10 +1,16 @@
 const sumAll = function(arg1, arg2) {
   let sum = 0;
-  for (let i = arg1 ; i <= arg2; i++) {
-    sum += (i);
+  if (arg1 < arg2) {
+    for (let i = arg1 ; i <= arg2; i++) {
+      sum += (i);
+    };
+    return sum;
+  }else {
+    for (let i = arg2 ; i <= arg1; i++) {
+      sum += (i);
+    };
+    return sum;
   };
-  return sum;
-
 };
 
 // Do not edit below this line
