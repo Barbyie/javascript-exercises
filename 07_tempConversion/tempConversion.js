6bb012b4b6c32133a41354918810c940a5bf3dc2
Barbyie <thiago.barbieri2002@gmail.com>
2024-02-08 21:25:04 -0300

    Pass all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
 const convertToCelsius = value => {
-  let fahrenheit = ((value - 32) * 5) / 9
+  let fahrenheit = ((value - 32) * 5) / 9;
   return Number(fahrenheit.toFixed(1));
 };
 
 const convertToFahrenheit = value => {
+  let celsius = (value * 1.8) + 32;
+  return Number(celsius.toFixed(1)); 
 };
 
 // Do not edit below this line
