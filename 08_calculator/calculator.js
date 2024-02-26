a270587eb857653b98beacec9987a1da5a9ca4d0
Barbyie <thiago.barbieri2002@gmail.com>
2024-02-26 17:35:21 -0300

    Create factorial function for the calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -28,8 +28,17 @@ const power = function(num1, num2) {
   return num1 ** num2;	
 };
 
-const factorial = function() {
-	
+const factorial = function(num1) {
+  array = new Array(num1);
+  if (num1 === 0) {
+    return 1;
+  }else {
+    let total = 1;
+    for (let i = 1; i  <= array.length; i++) {
+      total *= i ;
+    }
+    return total;
+  };	
 };
 
 // Do not edit below this line
